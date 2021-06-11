38abe687dd61513e5f3813db27fde8a436bb592b
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-11 10:29:05 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '07e4f5379700196362aaad5ffa54b04b8b4e64ce782402093478184e5cb5c8a5f1b3619e4be47cfc68073780e04b39569bd204dc9cb06c3f22276bcac154aa5c'
