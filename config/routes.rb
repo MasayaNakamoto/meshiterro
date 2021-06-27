aa8a303ac388fe60eb282005b4b16979838b8434
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-27 10:51:28 +0000

    hukusyu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+    # ユーザ認証機能
   devise_for :users
+#   これでここがメインになる
   root to: 'homes#top'
+#   resoucesで全てのルーティングができる
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
    resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
